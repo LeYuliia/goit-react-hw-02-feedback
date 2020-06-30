67a33cef8b2af19d56c9a3eb3be9968822a4d601
LeYuliia <legeza.y@gmail.com>
2020-06-30 14:17:08 +0300

    Updates

---
+++ precache-manifest.2cddf05d3219b6fd2e9bac5fad031fbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1238c555b9b56241e2098eb994b7bdfe",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "509a4634f185f572fead",
+    "url": "/goit-react-hw-02-feedback/static/css/main.904d7ffa.chunk.css"
+  },
+  {
+    "revision": "238103081858691ebdef",
+    "url": "/goit-react-hw-02-feedback/static/js/2.58d5af0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.58d5af0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509a4634f185f572fead",
+    "url": "/goit-react-hw-02-feedback/static/js/main.f6b37589.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
