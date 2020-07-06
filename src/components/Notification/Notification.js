1b0e2d6ff98014e5b297aca0de03059e844bc250
LeYuliia <legeza.y@gmail.com>
2020-07-06 11:09:59 +0300

    second

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -2,9 +2,7 @@ import React from "react";
 import styles from "./Notification.module.css";
 
 const Notification = ({ message }) => (
-  <>
-    <p className={styles.message}>{message}</p>
-  </>
+  <p className={styles.message}>{message}</p>
 );
 
 export default Notification;
